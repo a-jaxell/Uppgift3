3cd8e765f4524763b41ac3b4c087fe96df4c3470
KvDread <97404672+KvDread@users.noreply.github.com>
2022-09-16 15:18:30 +0200

    Add script.js delinked and commented out

---
+++ resources/javascript/script.js
@@ -0,0 +1,16 @@
+
+// deklarera variablen state
+//   let state = true;
+// Gör en funktion som ändrar state mellan true/false vid klick på knappen.
+
+//function changeState(){
+//    state =! state;
+//}
+//onclick="myFunction()"
+
+// Ändra texten till good bye world när state == false
+
+//function titleChange(){
+//    if let state 
+//}
+//document.getElementById("knapp").innerHTML = "Good Bye World";
