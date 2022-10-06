9cad3ea0df56a81a0bb7cba361a68caa39bbefce
KvDread <97404672+KvDread@users.noreply.github.com>
2022-10-06 09:28:29 +0200

    Add seperated script, selector and handler

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -14,3 +14,26 @@
 //    if let state 
 //}
 //document.getElementById("knapp").innerHTML = "Good Bye World";
+
+// Ny kod nedan
+
+       // Genom att tilldela variabeln title ett värde utanför funktionen changeTitle så undviker man att inget händer
+        // första gången man klickar på knappen.
+        let title = document.getElementById("title").innerHTML;
+        
+        //Selector and Event listener
+        const button = document.querySelector('#clickBtn');
+        button.addEventListener('click',changeTitle);
+
+        function changeTitle(){
+            // Ternary operator som jag först inte fick att fungera men som nu funkar magiskt.
+            // title = (title === "Hello World") ? title = "Goodbye World" : title = "Hello World";
+            
+            if (title == "Hello World"){
+               title = "Goodbye World";
+                } else {
+                    title = "Hello World";
+                }
+            return document.getElementById("title").innerHTML = title;
+        }
+        
